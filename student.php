53e341871540f5ba8ad73245b5aaf9ab96de490c
Roger Holden <r.holden@chi.ac.uk>
2021-03-17 15:43:42 +0000

    Student page now showing modules (not checked)

--- student.php
+++ student.php
@@ -36,12 +36,11 @@ $fullname =   htmlentities($row['name']) . ' ' . htmlentities($row['last_name'])
  
 
 // debugging - dont' delete this until final version
-$stmt->debugDumpParams();
+// $stmt->debugDumpParams();
  
 
 ?>
-<!-- main heading -->
-<!-- <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-left"> -->
+ 
 
 <div class="pricing-header px-3 pb-md-4  text-left">
 
@@ -49,36 +48,74 @@ $stmt->debugDumpParams();
     <!-- intro text -->
     <p class="lead">View details for an individual student.</p>
 </div>
-
-
+ 
 
 <div class="container">
     
 
 
-<div class="card-deck mb-3 text-center">
+<div class="tudent-card-deck mb-3 text-left">
         <!-- card one -->
         <div class="card mb-4 box-shadow">
             <div class="card-header">
-                <h4 class="my-0 font-weight-normal">Student <?php
-                echo  $fullname  ;
-
-            ?></h4>
+                <h4 class="my-0 font-weight-normal">Student <?php echo  $fullname;?></h4>
             </div>
             <div class="card-body">
 
-                <h1 class="card-title uni-card-title">Total students xxx </h1>
-  
+                <!-- <h1 class="card-title uni-card-title">Total students xxx </h1> -->
+            <h4>Department: <?php echo  $row['dept_name'];?></h4>
+            <h4>Total credit: <?php echo  $row['tot_cred'];?></h4>
+
+          
+
+        <?php
+
+            $stmt = $db->prepare("SELECT c.course_id, c.title, t.semester, t.year from student as s 
+            JOIN takes as t on t.ID = s.ID 
+            JOIN course as c on t.course_id = c.course_id
+            where s.ID = ? ORDER BY t.year");
+
+            $stmt->bindParam(1, $id);
+         
+            $stmt->execute();
+            
+            // debugging - dont' delete this until final version
+            // $stmt->debugDumpParams();
+            $totalrows = $stmt->rowCount();
+
+                  
+
+            ?>  
+            <h3>Modules (<?php echo $totalrows;?>)</h3>
+            <table class="table table-striped"> 
+
+            <?php
+
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+
+                echo '<tr><td>';
+
+                echo $row['title'];
+
+                echo '</td><td>';
+                
+                echo $row['semester'];
+                echo '</td><td>';
+                echo $row['year'];
+                echo '</td></tr>';
+
+            }
+            
+            // exit();
+
+        ?>
+        </table>
             </div>
       
              
             </div>
 
-         
-
-
           
-                <!-- <h1 class="card-title uni-card-title">Total students <?php echo  $totalrows;?></h1> -->
           
             
                 <a href="students.php?limit=9999999999"> <button type="button" class="btn btn-lg btn-block btn-outline-primary">See all students</button></a>
